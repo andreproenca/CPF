61d8497c78acb35a7d8fa44e913ce7b82ff8010b
Andre Proenca <38325168+andreproenca@users.noreply.github.com>
2022-10-17 17:59:38 -0300

    Add files via upload

---
+++ validateCPF.py
@@ -0,0 +1,34 @@
+#  CPF (Brazilian document for civilian)
+
+documentNumber = input(
+    'Type your CPF document (Only numbers): ')  # '32290143839'
+
+newCPF = documentNumber[:-2]
+reverse = 10
+total = 0
+
+for index in range(19):
+    if index > 8:
+        index -= 9
+
+    total += int(newCPF[index]) * reverse
+    #print(int(newCPF[index]), index, reverse)
+
+    reverse -= 1
+    if reverse < 2:
+        reverse = 11
+        d = 11 - (total % 11)
+
+        if d > 9:
+            d = 0
+        total = 0
+        newCPF += str(d)
+
+sequence = newCPF == str(newCPF[0]) * len(documentNumber)
+
+if documentNumber == newCPF and not sequence:
+    print('valid')
+else:
+    print('Invalid')
+
+print(newCPF)
